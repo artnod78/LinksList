e57b30be355b2d9527584b868d9bddb7591d2e6a
Artnod <artnod78@gmail.com>
2015-05-24 19:11:36 +0200

    t

--- www/index.php
+++ www/index.php
@@ -11,40 +11,42 @@ function test_url($url){
 }
 
 function gen_list_link($url){
-        $callStartTime = microtime(true);
-        $zoom = explode('&', explode('=', $_POST['IntelUrl'])[2])[0];
-        $list = explode('_', explode('=', $_POST['IntelUrl'])[3]);
-        $loop=1;
-        $list_data[0] = array('ordre', 'gps_source', 'gps_destination', 'source', 'destination', 'intel_url', 'gmap_source', 'gmap_destination');
-        foreach($list as $link){
-                if($loop < 10)
-                        $ordre = 'lien 0'.$loop;
-                else
-                        $ordre = 'lien '.$loop;
-
-                $src_x = explode(',', $link)[0];
-                $src_y = explode(',', $link)[1];
-                $dest_x = explode(',', $link)[2];
-                $dest_y = explode(',', $link)[3];
-                $gps_source = $src_x.','.$src_y;
-                $gps_destination = $dest_x.','.$dest_y;
-                $moy_x=($src_x+$dest_x)/2;
-                $moy_y=($src_y+$dest_y)/2;
-                $intel_url = 'https://www.ingress.com/intel?ll='.$moy_x.','.$moy_y.'&z='.$zoom.'&pls='.$link;
-                $gmap_source = 'https://www.google.fr/maps/search/'.$gps_source;
-                $gmap_destination = 'https://www.google.fr/maps/search/'.$gps_destination;
-                $list_data[$loop] = array($ordre, $gps_source, $gps_destination, 'A Remplir',  'A Remplir',  $intel_url, $gmap_source, $gmap_destination);
-                $loop++;
-        }
-        $callEndTime = microtime(true);
-        $callTime = $callEndTime - $callStartTime;
-        echo '<p>'.date('H:i:s').' Liste des liens genere en '.sprintf('%.4f',$callTime).' secondes<br>';
-		$nblink=count($list_data)-1;
-		echo date('H:i:s').' '.$nblink.' liens</p>';
-        return $list_data;
+    $callStartTime = microtime(true);
+    $zoom = explode('&', explode('=', $_POST['IntelUrl'])[2])[0];
+    $list = explode('_', explode('=', $_POST['IntelUrl'])[3]);
+    $loop=1;
+    $list_data[0] = array('ordre', 'gps_source', 'gps_destination', 'source', 'destination', 'intel_url', 'gmap_source', 'gmap_destination');
+    foreach($list as $link){
+        if($loop < 10){
+            $ordre = 'lien 0'.$loop;
+		}
+        else{
+            $ordre = 'lien '.$loop;
+		}
+        $src_x = explode(',', $link)[0];
+        $src_y = explode(',', $link)[1];
+        $dest_x = explode(',', $link)[2];
+        $dest_y = explode(',', $link)[3];
+        $gps_source = $src_x.','.$src_y;
+        $gps_destination = $dest_x.','.$dest_y;
+        $moy_x=($src_x+$dest_x)/2;
+        $moy_y=($src_y+$dest_y)/2;
+        $intel_url = 'https://www.ingress.com/intel?ll='.$moy_x.','.$moy_y.'&z='.$zoom.'&pls='.$link;
+        $gmap_source = 'https://www.google.fr/maps/search/'.$gps_source;
+        $gmap_destination = 'https://www.google.fr/maps/search/'.$gps_destination;
+        $list_data[$loop] = array($ordre, $gps_source, $gps_destination, 'A Remplir',  'A Remplir',  $intel_url, $gmap_source, $gmap_destination);
+        $loop++;
+    }
+    $callEndTime = microtime(true);
+    $callTime = $callEndTime - $callStartTime;
+    echo '<p>'.date('H:i:s').' Liste des liens genere en '.sprintf('%.4f',$callTime).' secondes<br>';
+	$nblink=count($list_data)-1;
+	echo date('H:i:s').' '.$nblink.' liens</p>';
+    return $list_data;
 }
 
 function gen_list_key($list){
+	$callStartTime = microtime(true);
 	$loop=0;
 	foreach($list as $link){
 		if($loop >= 1){
@@ -56,12 +58,17 @@ function gen_list_key($list){
 	$list_dest = array_keys($list_key);
 	$list_nbkey = array_values($list_key);
 	$loop=1;
+	$nbkey=count($list_key);
 	$list_data[0]= array('destination','nb cle','intel url');
-	while($loop < count($list_key)){
+	while($loop < $nbkey){
 		$list_data[$loop]= array($list_dest[$loop],$list_nbkey[$loop],'www.ingress.com/intel?pll='.$list_dest[$loop]);
 		$loop++;
 	}
-	print_r($list_data);
+	$callEndTime = microtime(true);
+    $callTime = $callEndTime - $callStartTime;
+    echo '<p>'.date('H:i:s').' Liste des cle genere en '.sprintf('%.4f',$callTime).' secondes<br>';
+	echo date('H:i:s').' '.$nbkey.' liens</p>';
+	return $list_data;
 }
 
 ?>
@@ -71,7 +78,7 @@ function gen_list_key($list){
 		echo '<p>'.date('H:i:s').' Url ok<br>';
 		echo date('H:i:s').'<a href="'.$_POST['IntelUrl'].'"> Url saisi</a></p>';
 		$list_link = gen_list_link($url);
-		gen_list_key($list_link);
+		$list_key=gen_list_key($list_link);
 		
 
 	}
